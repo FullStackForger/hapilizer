ad7008f6b63cfa5739c39bc0f6f50aa3f0bb169a
IndieForger <indieforger@gmail.com>
2016-04-08 19:09:00 +0100

    point client to right endpoint: /user/me

--- client/app/services/account.js
+++ client/app/services/account.js
@@ -2,10 +2,10 @@ angular.module('MyApp')
   .factory('Account', function($http) {
     return {
       getProfile: function() {
-        return $http.get('/api/me');
+        return $http.get('/user/me');
       },
       updateProfile: function(profileData) {
-        return $http.put('/api/me', profileData);
+        return $http.put('/user/me', profileData);
       }
     };
   });
