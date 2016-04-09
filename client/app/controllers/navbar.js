2340857fd6ca4d37e69ec29ed855204748f15b4d
IndieForger <indieforger@gmail.com>
2016-04-09 20:21:26 +0100

    modified client removed

--- client/app/controllers/navbar.js
+++
@@ -1,6 +0,0 @@
-angular.module('MyApp')
-  .controller('NavbarCtrl', function($scope, $auth, auth) {
-    $scope.isAuthenticated = function() {
-      return $auth.isAuthenticated() || auth.isAuthenticated();
-    };
-  });
