e3d5582de622e1bae0b6d198ae8e4751a125efb2
IndieForger <indieforger@gmail.com>
2016-07-19 20:35:13 +0100

    client src moved to ./example

--- example/server.js
+++ example/server.js
@@ -1,7 +1,7 @@
 const Hapi = require('hapi');
 
-const config = require('./config');
-const hapilizer = require('./lib/hapilizer');
+const config = require('./../config');
+const hapilizer = require('./../lib/hapilizer');
 
 const server = new Hapi.Server();
 const connection = {
